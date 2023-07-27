4b2fc7ee87b1d0887d6b74b1829ee145fd49dde0
Nicasiomarques <nicasiomarques18@gmail.com>
2023-07-27 18:57:29 +0100

    feat: add inputSearch component

---
+++ src/components/input-field/search-input.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent, useState } from "react";
+
+import { useDebounce } from "../../hooks/use-debounce";
+import { InputField, InputFieldProps } from "./base";
+
+type SearchInputProps = Omit<InputFieldProps, 'onChange'> & {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export const SearchInput: React.FC<SearchInputProps> = ({ value, onChange, ...rest }) => {
+  const [displayValue, setDisplayValue] = useState<string>(value);
+  const debouncedChange = useDebounce((value) => onChange(value), 500);
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const newValue = event.target.value;
+    setDisplayValue(newValue);
+    debouncedChange(newValue);
+  }
+
+  return (
+    <InputField
+      onChange={handleChange}
+      value={displayValue}
+      type="search"
+      {...rest}
+    />
+  );
+};
