2e960b4743eae0353cbaaa67b6a5b3a2e6fd07b2
Nicasiomarques <nicasiomarques18@gmail.com>
2023-07-27 18:59:00 +0100

    feat: add listAsGrid component component

---
+++ src/components/section-cards/index.tsx
@@ -0,0 +1,22 @@
+import { Card } from "../card"
+
+type AnimeItem = {
+  id: string,
+  thumb: string,
+  title: string
+}
+
+type ListAsGridProps = {
+  animeList: AnimeItem[]
+  isLoading: boolean
+}
+
+export const ListAsGrid = ({ animeList, isLoading }:ListAsGridProps) => {
+  return (
+    <section className="section-cards">
+      {isLoading && <span>Carregando...</span>}
+      {!isLoading && !animeList.length && <span>Nem um resgistro encontrado...</span>}
+      {animeList.map((anime) => <Card key={anime.id} {...anime} />)}
+    </section>
+  )
+}
