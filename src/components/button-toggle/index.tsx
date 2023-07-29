71d10b4acfa1f21187f60535fa0bd2fe4a855dc5
Nicasiomarques <nicasiomarques18@gmail.com>
2023-07-29 21:59:18 +0100

    refactor: make toggle button maintainable

--- src/components/button-toggle/index.tsx
+++ src/components/button-toggle/index.tsx
@@ -1,14 +1,24 @@
+import classnames from 'classnames'
+
 interface ButtonPropsToggle {
   isOn: boolean;
   toggle: () => void;
 }
 
 export const ButtonToggle: React.FC<ButtonPropsToggle> = ({ isOn, toggle }) => {
+  const btnStyles = {
+    base: 'py-2 px-4 font-bold border-2 rounded cursor-pointer border-purple-600',
+    on: 'bg-purple-600 text-white',
+    off: 'text-purple-600',
+  };
+
+  const buttonClass = classnames(btnStyles.base, {
+    [btnStyles.on]: isOn,
+    [btnStyles.off]: !isOn,
+  });
+
   return (
-    <button
-      className={`py-2 px-4 font-bold border-2 rounded cursor-pointer border-purple-600 ${isOn ? 'bg-purple-600 text-white' : 'text-purple-600 '}`}
-      onClick={toggle}
-    >
+    <button className={buttonClass} onClick={toggle}>
       {isOn ? 'Ascendente' : 'Descendente'}
     </button>
   );
