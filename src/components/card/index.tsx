650f9bfef14a766a0fb3faf45fcec7a3458c1f33
Nicasiomarques <nicasiomarques18@gmail.com>
2023-07-29 21:59:44 +0100

    refactor: make card component maintainable

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,15 +1,24 @@
 import { memo } from "react"
+import classnames from 'classnames'
 
 type CardProps = {
   thumb: string
   title: string
 }
 
-export const Card = memo(({ thumb, title }: CardProps) => {
+export const Card: React.FC<CardProps> = memo(({ thumb, title }) => {
+  const cardStyles = {
+    base: 'group rounded hover:scale-105 cursor-pointer duration-200',
+    image: 'rounded-md brightness-75 duration-200 hover:brightness-100',
+    title: 'text-amber-600 text-sm truncate mt-2 group-hover:text-purple-600',
+  };
+
+  const cardClass = classnames(cardStyles.base);
+
   return (
-    <article className='group rounded hover:scale-105 cursor-pointer duration-200'>
-      <img className='rounded-md brightness-75 duration-200 hover:brightness-100' src={thumb} alt={title} />
-      <h3 className="text-amber-600 text-sm truncate mt-2 group-hover:text-purple-600">{title}</h3>
+    <article className={cardClass}>
+      <img className={cardStyles.image} src={thumb} alt={title} />
+      <h3 className={cardStyles.title}>{title}</h3>
     </article>
-  )
-})
+  );
+});
