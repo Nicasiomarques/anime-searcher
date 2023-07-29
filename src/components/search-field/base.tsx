833954d6ebf5cc9e6f1befa13bc5fac4eadb830d
Nicasiomarques <nicasiomarques18@gmail.com>
2023-07-29 17:28:11 +0100

    refactor: change to tailwind style input field

--- src/components/search-field/base.tsx
+++ src/components/search-field/base.tsx
@@ -4,7 +4,9 @@ import './input-field.styles.css'
 export type InputFieldProps = HTMLProps<HTMLInputElement> & { label?: string }
 
 export const InputField: React.FC<InputFieldProps> = ({ label,...rest }) =>
-  <div className="input-wrapper">
-    {label && <label className="input-field__label">{label}</label>}
-    <input type="text" {...rest} />
+  <div className="">
+    {label && <label className="font-bold text-base capitalize text-gray-600">{label}</label>}
+    <input type="text"
+    className="bg-white rounded px-4 py-3 w-full shadow-sm shadow-slate-300 focus:shadow-md focus:-translate-y-1 outline-none transition-all duration-200"
+    {...rest} />
   </div>
