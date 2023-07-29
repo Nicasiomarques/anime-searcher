0fff51b99d90b333e7c9513769726f0ea878ce92
Nicasiomarques <nicasiomarques18@gmail.com>
2023-07-29 22:00:12 +0100

    refactor: make btn item maintainable

---
+++ src/components/pagination/btn-item.tsx
@@ -0,0 +1,26 @@
+import classnames from 'classnames'
+
+interface BtnItemProps {
+  onPageChange: () => void;
+  isOn: boolean;
+  label: string | number
+}
+
+export const BtnItem = ({ onPageChange, isOn, label }: BtnItemProps) => {
+  const btnStyles = {
+    base: 'border-2 border-transparent rounded px-4 py-1 duration-200',
+    off: 'hover:border-purple-600 hover:text-purple-600',
+    on: 'bg-purple-600 text-white',
+  };
+
+  const buttonClass = classnames(btnStyles.base, {
+    [btnStyles.on]: isOn,
+    [btnStyles.off]: !isOn,
+  });
+
+  return (
+    <button onClick={onPageChange} className={buttonClass}>
+      {label}
+    </button>
+  )
+}
