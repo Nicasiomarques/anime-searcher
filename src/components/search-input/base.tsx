3fbfa7fd402417001aa0ddcae54d0b70453abda8
Nicasiomarques <nicasiomarques18@gmail.com>
2023-07-29 22:03:26 +0100

    style: add wrapper style input field

--- src/components/search-input/base.tsx
+++ src/components/search-input/base.tsx
@@ -1,12 +1,9 @@
 import { HTMLProps } from "react"
-import './input-field.styles.css'
 
 export type InputFieldProps = HTMLProps<HTMLInputElement> & { label?: string }
 
 export const InputField: React.FC<InputFieldProps> = ({ label,...rest }) =>
-  <div className="">
+  <div className="flex flex-col gap-2">
     {label && <label className="font-bold text-base capitalize text-gray-600">{label}</label>}
-    <input type="text"
-    className="bg-white rounded px-4 py-3 w-full shadow-sm shadow-slate-300 focus:shadow-md focus:-translate-y-1 outline-none transition-all duration-200"
-    {...rest} />
+    <input type="text" className="bg-white rounded px-4 py-3 w-full shadow-sm shadow-slate-300 focus:shadow-md focus:-translate-y-1 outline-none transition-all duration-200" {...rest} />
   </div>
