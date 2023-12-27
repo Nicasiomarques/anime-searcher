f57dec0f0df253931b5da08516f00e0ea9d8fb32
Nicasiomarques <nicasiomarques18@gmail.com>
2023-12-27 13:06:23 +0100

    refactor: replace loading default by daisyui loading

--- src/components/section-grid/index.tsx
+++ src/components/section-grid/index.tsx
@@ -12,7 +12,7 @@ type ListAsGridProps = {
 export const SectionGrid = ({ animeList, isLoading }: ListAsGridProps) => {
   return (
     <section className="grid grid-cols-4-full gap-6 justify-items-center my-8">
-      {isLoading && <span>Carregando...</span>}
+      {isLoading && <span className="loading loading-ring loading-lg"></span>}
       {!isLoading && !animeList.length && <span>Nem um resgistro encontrado...</span>}
       {animeList.map(anime => (
         <Card key={anime.id} {...anime} />
